d88f05a02e05de51a1f75545a852111547701f5b
Craig McKeachie <craigdmckeachie@gmail.com>
2025-12-01 13:53:25 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/App.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class App {
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/sakila";
+        String username = "root";
+        String password = "";
+
+        //1. open a connection to the database
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+    }
+}
