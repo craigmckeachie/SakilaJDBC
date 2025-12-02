4f9efe0b386858a14d1de3f638fa8f1b31dade4e
Craig McKeachie <craigdmckeachie@gmail.com>
2025-12-02 14:34:07 -0500

    try with resources

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -4,50 +4,43 @@ import java.sql.*;
 
 public class App {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
+
         String url = "jdbc:mysql://localhost:3306/sakila";
         String username = args[0];
         String password = args[1];
 
-        //1. open a connection to the database
-        Connection connection = DriverManager.getConnection(url, username, password);
-
-
-        // define your query
         String query = """
-                        SELECT title, description, release_year, length
-                        FROM film
-                        WHERE title LIKE ?
-                        """;
+                SELECT title, description, release_year, length
+                FROM film
+                WHERE title LIKE ?
+                """;
 
-        // create statement
-        // the statement is tied to the open connection
-        PreparedStatement statement = connection.prepareStatement(query);
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             PreparedStatement statement = connection.prepareStatement(query)) {
 
-        String searchTerm = "%AIR%";
-        //set parameters
-        statement.setString(1,  searchTerm);
+            String searchTerm = "%AIR%";
+            statement.setString(1, searchTerm);
 
-        // 2. Execute your query
-        ResultSet results = statement.executeQuery();
+            try (ResultSet results = statement.executeQuery()) {
 
-       // process the results
-        while (results.next()) {
-            String title = results.getString("title");
-            String description = results.getString("description");
-            int releaseYear = results.getInt("release_year");
-            int length = results.getInt("length");
+                while (results.next()) {
+                    String title = results.getString("title");
+                    String description = results.getString("description");
+                    int releaseYear = results.getInt("release_year");
+                    int length = results.getInt("length");
 
-            System.out.println(title);
-            System.out.println(description);
-            System.out.println(releaseYear);
-            System.out.println(length);
-            System.out.println("-----------------------");
+                    System.out.println(title);
+                    System.out.println(description);
+                    System.out.println(releaseYear);
+                    System.out.println(length);
+                    System.out.println("-----------------------");
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("There was an error retrieving the data. Please try again.");
+            e.printStackTrace();
         }
-
-        results.close();
-        statement.close();
-        connection.close();
-
     }
 }
